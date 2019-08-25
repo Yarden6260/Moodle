5cbe96e639c1ff8da3a5e5154b4c02a490daa8d8
Yarden6260 <30807572+Yarden6260@users.noreply.github.com>
2019-08-25 15:21:36 +0300

    Update setup_webserver.sh

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -60,7 +60,7 @@ check_fileServerType_param $fileServerType
     #configure gluster repository & install gluster client
     sudo add-apt-repository ppa:gluster/glusterfs-3.10 -y
     sudo add-apt-repository ppa:ondrej/php -y
-    sudo apt-get -y update
+    sudo apt-get -y update && apt -y upgrade
     sudo apt-get -y install glusterfs-client
   elif [ "$fileServerType" = "azurefiles" ]; then
     sudo apt-get -y install cifs-utils
